e8058108873b6959aa4ff14529014e6dd67837d3
Даня Гребнев <naruximus@gmail.com>
2021-01-10 22:35:07 +0300

    initial settings

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './App.modules.scss'
+
+const App = () => {
+  return (
+    <div className={style.header}>
+      Yeah, This is App Component
+    </div>
+  )
+}
+
+export default App
