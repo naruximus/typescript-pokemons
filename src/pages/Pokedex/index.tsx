b2ca1e2e9a1cd42c7508e8e865bddb4d0680bbdb
Danila Grebnev <naruximus@icloud.com>
2021-02-22 23:22:02 +0300

    Add debounce hook

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import style from './Pokedex.module.scss';
 import { Heading, Footer } from '../../components';
 import { PokemonCard } from './components';
 import { useData } from '../../hooks/getData';
 
-// import { IPokemons } from './pokemons';
 import { IPokemons, IPokemon } from '../../interface/pokemons';
+import { useDebounce } from '../../hooks/useDebounce';
+import { Pokeball } from './assets';
 
 interface IQuery {
   name?: string;
@@ -17,7 +18,9 @@ export const Pokedex = () => {
   const [searchValue, setSearchValue] = useState('');
   const [query, setQuery] = useState<IQuery>({ limit: 9 });
 
-  const { data, isLoading, isError } = useData<IPokemons>('getPokemons', query, [searchValue]);
+  const debouncedValue = useDebounce(searchValue, 500);
+
+  const { data, isLoading, isError } = useData<IPokemons>('getPokemons', query, [debouncedValue]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
@@ -45,9 +48,13 @@ export const Pokedex = () => {
           placeholder="Encuentra tu pokémon..."
           onChange={handleSearchChange}
         />
-        {!isLoading &&
-          data &&
-          data.pokemons.map((pokemon: IPokemon) => <PokemonCard key={pokemon.id} pokemon={pokemon} />)}
+        {!isLoading && data ? (
+          data.pokemons.map((pokemon: IPokemon) => <PokemonCard key={pokemon.id} pokemon={pokemon} />)
+        ) : (
+          <div className={style.loaderWrap}>
+            <img src={Pokeball} alt="Loading..." />
+          </div>
+        )}
       </div>
       <Footer />
     </div>
