64f4894563d5c4413ebfcd8cf7387825657f9aef
Danila Grebnev <naruximus@icloud.com>
2021-02-22 23:30:17 +0300

    Style and Error fixes

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -3,7 +3,7 @@ import { config } from '../config';
 export const getUrlWithParamsConfig = (endPointConfig: string, query: object) => {
   const url = {
     ...config.client.server,
-    ...config.client.endpoint[endPointConfig].uri,
+    ...(config.client.endpoint as any)[endPointConfig].uri,
     query,
   };
 
