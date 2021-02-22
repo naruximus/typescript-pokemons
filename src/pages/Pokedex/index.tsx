7fb854b5dce73e488e368fe2023647389a774a66
= <=>
2021-02-22 15:46:03 +0300

    Add Footer on PokedexPage

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Header } from '../../components';
+import { Header, Footer } from '../../components';
 
 export const Pokedex = () => (
   <>
     <Header />
     <div>Pokedex</div>
+    <Footer />
   </>
 );
