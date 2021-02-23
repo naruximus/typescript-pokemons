ae062722fffb65b6b36b5f3ea2aedafff493dada
Danila Grebnev <naruximus@icloud.com>
2021-02-23 15:48:20 +0300

    Add deps for testing. Create example test

---
+++ example.test.ts
@@ -0,0 +1,10 @@
+describe('Example Test', () => {
+  test('Должен вернуть 2 при сложении 1 и 1', () => {
+    expect(1 + 1).toBe(2);
+  })
+  describe('Example Test', () => {
+    test('Должен вернуть 2 при сложении 1 и 1', () => {
+      expect(1 + 1).toBe(2);
+    })
+  })
+})
